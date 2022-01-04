efa51d29cfd2036a5c5b62c43a0b3f9de76a805a
bayashi <yossiwimps@gmail.com>
2022-01-04 17:27:09 +0900

    【add】number-string.ts

---
+++ src/number-string.ts
@@ -0,0 +1,14 @@
+/*
+  number_string型についてのプラクティス
+  @data: 2022/01/04
+*/
+export {};
+
+let year: number = 1996;
+console.log(year);
+
+let age: number = 0x2b;
+console.log(age);
+
+let name: string = 'baya';
+console.log(name);
