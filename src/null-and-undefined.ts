9d371029889962b7a27add5d4c11480a1cef08b2
bayashi <yossiwimps@gmail.com>
2022-01-06 19:08:07 +0900

    【add】null-and-undefined.ts

---
+++ src/null-and-undefined.ts
@@ -0,0 +1,11 @@
+/*
+  null_undefindeについてのプラクティス
+  @data: 2022/01/06
+*/
+export {};
+
+// 変な感じだがNULL型 NULL以外を許容しない
+let absence: null = null;
+
+// undefinde型 undefined以外は許容しない
+let data: undefined = undefined;
